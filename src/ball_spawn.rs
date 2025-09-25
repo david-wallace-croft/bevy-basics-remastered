69eaaade0869512238311c1596ffef542829985c
David Wallace Croft <david@CroftSoft.com>
2025-09-24 19:16:03 -0500

    power

--- src/ball_spawn.rs
+++ src/ball_spawn.rs
@@ -7,27 +7,41 @@ use ::bevy::window::PrimaryWindow;
 #[derive(Event)]
 pub struct BallSpawn {
   position: Vec3,
+  power: f32,
   velocity: Vec3,
 }
 
 pub fn shoot_ball(
   input: Res<ButtonInput<MouseButton>>,
   player: Single<&Transform, With<Player>>,
+  mut power: Local<Option<f32>>,
   mut spawner: EventWriter<BallSpawn>,
+  time: Res<Time>,
   window: Single<&Window, With<PrimaryWindow>>,
 ) {
   if window.cursor_options.visible {
     return;
   }
 
-  if !input.just_pressed(MouseButton::Left) {
-    return;
+  if let Some(current) = power.as_mut() {
+    if input.just_released(MouseButton::Left) {
+      spawner.write(BallSpawn {
+        position: player.translation,
+        power: *current,
+        velocity: player.forward().as_vec3() * 15.,
+      });
+    }
+
+    if input.pressed(MouseButton::Left) {
+      *current += time.delta_secs();
+    } else {
+      *power = None
+    }
   }
 
-  spawner.write(BallSpawn {
-    position: player.translation,
-    velocity: player.forward().as_vec3() * 15.,
-  });
+  if input.just_pressed(MouseButton::Left) {
+    *power = Some(1.);
+  }
 }
 
 pub fn spawn_ball(
@@ -40,7 +54,7 @@ pub fn spawn_ball(
       Transform::from_translation(spawn.position),
       Mesh3d(ball_data.mesh()),
       MeshMaterial3d(ball_data.material()),
-      Velocity(spawn.velocity),
+      Velocity(spawn.velocity * spawn.power * 10.),
     ));
   }
 }
